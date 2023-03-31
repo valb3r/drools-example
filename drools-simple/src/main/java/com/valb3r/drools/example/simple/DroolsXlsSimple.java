2874ed5d27d4cf27397ce3753f6e337be0caa687
valb3r <valb3r.ua@gmail.com>
2023-03-31 12:13:13 +0300

    Added comments

--- drools-simple/src/main/java/com/valb3r/drools/example/simple/DroolsXlsSimple.java
+++ drools-simple/src/main/java/com/valb3r/drools/example/simple/DroolsXlsSimple.java
@@ -32,21 +32,24 @@ public class DroolsXlsSimple {
     public static void main(String[] args) {
         var csv = readInputCsv(new File("drools-simple/src/main/resources/example.csv"));
 
+        // Build Drools services and container
         KieServices services = KieServices.Factory.get();
         KieFileSystem fileSystem = services.newKieFileSystem();
-        var droolsXlsFilePath = "drools-simple/src/main/resources/taxes-rules.xlsx";
+        var droolsXlsFilePath = "drools-simple/src/main/resources/taxes-rules.xlsx"; // XLSX file with rules
+        // Write XLSX file into Drools working memory:
         fileSystem.write("src/main/resources/" + droolsXlsFilePath + ".drl.xlsx", Files.readAllBytes(Path.of(droolsXlsFilePath)));
+        // Create Kie module:
         KieBuilder kb = services.newKieBuilder(fileSystem);
         kb.buildAll();
         KieModule kieModule = kb.getKieModule();
-        var container = services.newKieContainer(kieModule.getReleaseId());
+        var container = services.newKieContainer(kieModule.getReleaseId()); // We can use this container for rule execution
 
         System.out.println();
         System.out.println();
         System.out.println("==================== Drools mapping is: ====================");
 
         for (Map<String, Object> stringObjectMap : csv) {
-            var session = container.newStatelessKieSession();
+            var session = container.newStatelessKieSession(); // Create new session for each row. Since session is stateless, you can move this out of loop and share session with every object
             var droolsMappedOutput = new HashMap<String, Object>();
             session.execute(List.of(new Input(stringObjectMap), new Output(droolsMappedOutput)));
             System.out.printf("For %s %n", stringObjectMap);
